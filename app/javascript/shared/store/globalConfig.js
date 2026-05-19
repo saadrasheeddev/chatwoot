31ad864c597412b6cd70d08c20741c18cfb45061
Saad Rasheed <84860156+saadrasheeddev@users.noreply.github.com>
2026-05-19 14:33:40 +0500

    feat: implement new design system color palette and add automated account management notification mailers

--- app/javascript/shared/store/globalConfig.js
+++ app/javascript/shared/store/globalConfig.js
@@ -56,8 +56,8 @@ const state = {
 export const getters = {
   get: $state => $state,
   isOnChatwootCloud: $state => $state.deploymentEnv === 'cloud',
-  isACustomBrandedInstance: $state => $state.installationName !== 'Chatwoot',
-  isAChatwootInstance: $state => $state.installationName === 'Chatwoot',
+  isACustomBrandedInstance: $state => $state.installationName !== 'DealDesk',
+  isAChatwootInstance: $state => $state.installationName === 'DealDesk',
 };
 
 export const actions = {};
